d59d1e28829ac7c442071df9a51bca26610cf892
Mahmoud Saad <msaaad202020@gmail.com>
2025-05-19 14:37:37 +0300

    Add MovieList Cell

---
+++ PaymobMovieList/PaymobMovieList/Scene/MovieList/View/Cell/View/MovieListCell.swift
@@ -0,0 +1,86 @@
+//
+//  MovieListCell.swift
+//  PaymobMovieList
+//
+//  Created by Mahmoud Saad on 19/05/2025.
+//
+
+import UIKit
+import Combine
+
+class MovieListCell: UITableViewCell {
+
+    
+    @IBOutlet weak var containerView: UIView!
+
+    @IBOutlet private var movieImageView: UIImageView!
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var dateLabel: UILabel!
+    @IBOutlet private var voteCountLabel: UILabel!
+    @IBOutlet private var favoriteButton: UIButton!
+    @IBOutlet private var ratingLabel: UILabel!
+    
+    weak var delegate: MovieListCellDelegate?
+    
+    var viewModel: MovieListCellViewModel? {
+        didSet {
+            guard let viewModel = viewModel else { return }
+            configure(viewModel: viewModel)
+        }
+    }
+
+    var favoriteButtonTapped = PassthroughSubject<Movie, Never>()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+        containerView.cornerRadius = 10
+        containerView.borderWidth = 1
+        containerView.borderColor = .systemBlue
+        containerView.backgroundColor = .clear
+        movieImageView.cornerRadius = 8
+        titleLabel.textColor = .systemBlue
+        [dateLabel, voteCountLabel, ratingLabel].forEach {
+            $0.textColor = .systemGray
+        }
+        
+    }
+
+    public func configure(viewModel: MovieListCellViewModel) {
+        
+        Task { [weak self] in
+              await self?.loadImage(imageURL: viewModel.imageURL)
+          }
+        titleLabel.text = viewModel.title
+        dateLabel.text = viewModel.date
+        voteCountLabel.text = viewModel.voteCount
+        ratingLabel.text = viewModel.rating
+        favoriteButton.isSelected = viewModel.movie.isFavorite
+        updateFavoriteButtonAppearance(isFavorite: viewModel.movie.isFavorite)
+    }
+    
+    @IBAction func didTapFavoriteButton(_ sender: UIButton) {
+        delegate?.didTapFavoriteButton(in: self)
+    }
+    
+    @MainActor
+    func loadImage(imageURL: String) async {
+        let image = await AsyncImageFetcher.loadImage(from: imageURL)
+        movieImageView.image = image ?? UIImage(named: "image_placeholder")
+    }
+    
+    func updateFavoriteButtonAppearance(isFavorite: Bool) {
+        favoriteButton.isSelected = isFavorite
+        if isFavorite {
+            favoriteButton.setImage(UIImage(systemName: "heart.fill"), for: .selected)
+        } else {
+            favoriteButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        }
+    }
+    
+  
+}
